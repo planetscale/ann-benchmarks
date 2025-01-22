043a46d5f2089e8ef773455ae39af2c0654ec0f3
Vicent Marti <vmg@strn.cat>
2025-01-22 17:35:14 +0100

    sptag: build an actual SPANN index
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- ann_benchmarks/algorithms/sptag/module.py
+++ ann_benchmarks/algorithms/sptag/module.py
@@ -4,22 +4,44 @@ from ..base.module import BaseANN
 
 
 class Sptag(BaseANN):
-    def __init__(self, metric, algo):
-        self._algo = str(algo)
+    def __init__(self, metric, selectHead):
+        self._selectHead = str(selectHead)
         self._metric = {"angular": "Cosine", "euclidean": "L2"}[metric]
 
     def fit(self, X):
-        self._sptag = SPTAG.AnnIndex(self._algo, "Float", X.shape[1])
-        self._sptag.SetBuildParam("NumberOfThreads", "32", "Index")
-        self._sptag.SetBuildParam("DistCalcMethod", self._metric, "Index")
+        self._sptag = SPTAG.AnnIndex('SPANN', "Float", X.shape[1])
+
+        # Set the thread number to speed up the build procedure in parallel 
+        self._sptag.SetBuildParam("IndexAlgoType", "BKT", "Base")
+        self._sptag.SetBuildParam("IndexDirectory", "spann_index", "Base")
+        self._sptag.SetBuildParam("DistCalcMethod", "L2", "Base")
+
+        self._sptag.SetBuildParam("isExecute", "true", "SelectHead")
+        self._sptag.SetBuildParam("NumberOfThreads", "32", "SelectHead")
+        self._sptag.SetBuildParam("Ratio", "0.2", "SelectHead") # index.SetBuildParam("Count", "200", "SelectHead")
+        self._sptag.SetBuildParam("SelectHeadType", self._selectHead, "SelectHead")
+
+        self._sptag.SetBuildParam("isExecute", "true", "BuildHead")
+        self._sptag.SetBuildParam("RefineIterations", "3", "BuildHead")
+        self._sptag.SetBuildParam("NumberOfThreads", "32", "BuildHead")
+
+        self._sptag.SetBuildParam("isExecute", "true", "BuildSSDIndex")
+        self._sptag.SetBuildParam("BuildSsdIndex", "true", "BuildSSDIndex")
+        self._sptag.SetBuildParam("PostingPageLimit", "12", "BuildSSDIndex")
+        self._sptag.SetBuildParam("SearchPostingPageLimit", "12", "BuildSSDIndex")
+        self._sptag.SetBuildParam("NumberOfThreads", "32", "BuildSSDIndex")
+        self._sptag.SetBuildParam("InternalResultNum", "32", "BuildSSDIndex")
+        self._sptag.SetBuildParam("SearchInternalResultNum", "64", "BuildSSDIndex")
+
         self._sptag.Build(X, X.shape[0], False)
 
     def set_query_arguments(self, MaxCheck):
         self._maxCheck = MaxCheck
-        self._sptag.SetSearchParam("MaxCheck", str(self._maxCheck), "Index")
+        self._sptag.SetSearchParam("MaxCheck", str(self._maxCheck), "BuildHead")
+        self._sptag.SetSearchParam("MaxCheck", str(self._maxCheck), "BuildSSDIndex")
 
     def query(self, v, k):
         return self._sptag.Search(v, k)[0]
 
     def __str__(self):
-        return "Sptag(metric=%s, algo=%s, check=%d)" % (self._metric, self._algo, self._maxCheck)
+        return "Sptag(metric=%s, selectHead=%s, check=%d)" % (self._metric, self._selectHead, self._maxCheck)
